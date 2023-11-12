897ce9826ca510300990230ab4e7eed10fc4099b
mirzaa5 <150314717+mirzaa5@users.noreply.github.com>
2023-11-12 15:24:27 -0500

    Files ad
    Counter app files.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let savedEntry =  " - "+count 
+    saveEl.textContent += savedEntry
+    count = 0
+    countEl.textContent = count
+    
+    }
